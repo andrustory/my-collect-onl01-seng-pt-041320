3222ee5170d325c0be76181b0b36f4d4ab94806a
Andres Carvajal <jc1993box@hotmail.com>
2020-04-25 03:45:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,11 @@
 def my_collect (collection)
-  
+  counter =  0
+  yield array [counter]
+  counter =+ 1
+end
+
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
 end
