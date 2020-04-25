e252ea784a9392d9c6a802793b44c91f0ff6252f
Andres Carvajal <jc1993box@hotmail.com>
2020-04-25 04:10:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect (collection)
   counter =  0
-  new_collection = []
-  while counter <array.lenth
-  new_collection << yield (collection [counter])
-  counter += 1
-end
-new_collection
+   new_collection = []
+   while counter < collection.length
+      new_collection << yield (collection [counter])
+    counter += 1
+    end
+  new_collection
 end
 
 
