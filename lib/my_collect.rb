2dcfe85e3f0fcdf201bc8d6a0182ad86a2cb2d06
Andres Carvajal <jc1993box@hotmail.com>
2020-04-25 04:20:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect (collection)
+  counter =  0
+   new_collection = []
+   while counter < collection.length
+      new_collection << yield(collection[counter])
+      yield(collection[counter])
+    counter += 1
+    end
+  new_collection
+end
 
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
